2b99c03c365f01b72744a43c9ec9e6ef0bbecbaa
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-10-13 19:53:05 +0100

    Move request handlers to separate file

--- api/routes/adviceRoutes.js
+++ api/routes/adviceRoutes.js
@@ -1,28 +1,8 @@
 'use strict';
 
-const axios = require('axios');
+const adviceController = require('../controllers/adviceController');
 
 module.exports = (app) => {
-  app.route('/advice').get(async (req, res) => {
-    try {
-      const externalApiResponse = await axios.get('https://api.adviceslip.com/advice');
-      const advice = externalApiResponse.data.slip.advice;
-      
-      res.json(advice);
-    } catch (error) {
-      res.status(500).json(error);
-    }
-  });
-  
-  app.route('/advice/search/:searchTerm').get(async (req, res) => {
-    try {
-      const searcTerm = req.params.searchTerm;
-      const externalApiResponse = await axios.get(`https://api.adviceslip.com/advice/search/${searcTerm}`);
-      const advices = externalApiResponse.data.slips.map(slip => slip.advice);
-    
-      res.json(advices); 
-    } catch (error) {
-      res.status(500).json(error);
-    }
-  });
+  app.route('/advice').get(adviceController.getAdvice);
+  app.route('/advice/search/:searchTerm').get(adviceController.search);
 }
