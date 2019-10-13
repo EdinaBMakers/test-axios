c3fada73a9c0d9332ed658a54ed018a467c0ba2c
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-10-13 17:18:29 +0100

    Test and implement /advice happy path

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 'use strict';
 
 const express = require('express');
+const axios = require('axios');
 const app = express();
 
-app.route('/advice').get((req, res) => {
-  res.status(200).send();
+app.route('/advice').get(async (req, res) => {
+  const externalApiResponse = await axios.get('https://api.adviceslip.com/advice');
+  const advice = externalApiResponse.data.slip.advice;
+  
+  res.json(advice);
 });
 
 module.exports = app;
--- tests/routes.test.js
+++ tests/routes.test.js
@@ -2,14 +2,37 @@
 
 const request = require('supertest');
 const app = require('../app');
+const axios = require('axios');
+
+jest.mock('axios');
+
+function externalAdviceResponse(expectedAdvice) {
+  axios.get.mockImplementation(() => {
+    return Promise.resolve({
+      data: {slip: {advice: expectedAdvice}}});
+  });
+}
 
 describe('Routes', () => {
   describe('/advice', () => {
     test('It can handle request', (done) => {
+      externalAdviceResponse('');
+
       request(app).get('/advice').then((response) => {
         expect(response.status).toStrictEqual(200);
         done();
       });
+    });
+
+    test('It can respond with an advice', (done) => {
+      const expectedAdvice = 'Some people would be better off if they took their own advice.';
+      
+      externalAdviceResponse(expectedAdvice);
+
+      request(app).get('/advice').then((response) => {
+        expect(response.body).toStrictEqual(expectedAdvice);
+        done();
+      })
     });
   });
 });
