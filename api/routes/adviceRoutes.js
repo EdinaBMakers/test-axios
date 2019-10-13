a83a42019943645f8fdd9dc7c1e88b499ef18149
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-10-13 19:47:06 +0100

    Move routes to separate file

---
+++ api/routes/adviceRoutes.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const axios = require('axios');
+
+module.exports = (app) => {
+  app.route('/advice').get(async (req, res) => {
+    try {
+      const externalApiResponse = await axios.get('https://api.adviceslip.com/advice');
+      const advice = externalApiResponse.data.slip.advice;
+      
+      res.json(advice);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+  
+  app.route('/advice/search/:searchTerm').get(async (req, res) => {
+    try {
+      const searcTerm = req.params.searchTerm;
+      const externalApiResponse = await axios.get(`https://api.adviceslip.com/advice/search/${searcTerm}`);
+      const advices = externalApiResponse.data.slips.map(slip => slip.advice);
+    
+      res.json(advices); 
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+}
--- app.js
+++ app.js
@@ -1,30 +1,9 @@
 'use strict';
 
 const express = require('express');
-const axios = require('axios');
+const routes = require('./api/routes/adviceRoutes')
 const app = express();
 
-app.route('/advice').get(async (req, res) => {
-  try {
-    const externalApiResponse = await axios.get('https://api.adviceslip.com/advice');
-    const advice = externalApiResponse.data.slip.advice;
-    
-    res.json(advice);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
-
-app.route('/advice/search/:searchTerm').get(async (req, res) => {
-  try {
-    const searcTerm = req.params.searchTerm;
-    const externalApiResponse = await axios.get(`https://api.adviceslip.com/advice/search/${searcTerm}`);
-    const advices = externalApiResponse.data.slips.map(slip => slip.advice);
-  
-    res.json(advices); 
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+routes(app);
 
 module.exports = app;
